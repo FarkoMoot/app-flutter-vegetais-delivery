bb0dffb7b3cf40a855ebf135f7c3ca47ab6614f6
Farko Moot <farko_moot@hotmail.com>
2024-01-31 18:55:12 -0300

    add screen cart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
+import 'dart:async';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:projeto_frutas/src/home_screen.dart';
+import 'package:projeto_frutas/src/cart_screen.dart';
 
 //Screens
 import 'package:projeto_frutas/src/splash_screen.dart';
 import 'package:projeto_frutas/src/intro_screen.dart';
 import 'package:projeto_frutas/src/login_sreen.dart';
+import 'package:projeto_frutas/src/home_screen.dart';
+import 'package:projeto_frutas/src/items_screen.dart';
 
 //Widgets
 import 'package:projeto_frutas/src/Widget/box_sell.dart';
@@ -29,120 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const ItemsScreen(),
-    );
-  }
-}
-
-class ItemsScreen extends StatelessWidget {
-  const ItemsScreen({super.key});
-
-  @override
-  Widget build(BuildContext context){
-
-    return Scaffold(
-      appBar: AppBar(
-        leading: Container(
-          margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
-          height: 44,
-          width: 68,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30),
-            border: Border.all(width: 1, color: Color(0xffcccccc)),
-          ),
-          child: Icon(
-            Icons.arrow_back_ios_new,
-          ),
-        ),
-        title: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Vegetais ',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-              ),
-            ),
-            Image(
-              width: 16,
-              height: 16,
-              image: AssetImage('images/milho.png'),
-            ),
-          ],
-        ),
-        actions: [
-          Container(
-            height: 54,
-            width: 54,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              border: Border.all(width: 1, color: Color(0xffcccccc)),
-            ),
-            child: Icon(
-              Icons.search
-            ),
-          )
-        ],
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(24, 32, 24, 0),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                BoxSell(
-                  product: 'Pimentao',
-                  price: '6',
-                  img: AssetImage('images/box_sell/pimentao.png'),
-                ),
-                SizedBox(width: 16,),
-                BoxSell(
-                  product: 'Gengibre',
-                  price: '4',
-                  img: AssetImage('images/box_sell/gengibre.png'),
-                ),
-              ]
-            ),
-            SizedBox(height: 16,),
-            Row(
-              children: [
-                BoxSell(
-                  product: 'Couve',
-                  price: '2',
-                  img: AssetImage('images/box_sell/couve.png'),
-                ),
-                SizedBox(width: 16,),
-                BoxSell(
-                  product: 'Abobora',
-                  price: '8',
-                  img: AssetImage('images/box_sell/abobora.png'),
-                ),
-              ]
-            ),
-            SizedBox(height: 16,),
-            Row(
-              children: [
-                BoxSell(
-                  product: 'Cenoura',
-                  price: '4',
-                  img: AssetImage('images/box_sell/cenoura.png'),
-                ),
-                SizedBox(width: 16,),
-                BoxSell(
-                  product: 'Brocolis',
-                  price: '2',
-                  img: AssetImage('images/box_sell/brocolis.png'),
-                ),
-              ]
-            ),
-            SizedBox(height: 16,),
-            
-          ],
-        ),
-      ),
+      home: const CartScreen(),
     );
   }
 }
---
+++ lib/src/cart_screen.dart
@@ -0,0 +1,200 @@
+import 'package:flutter/material.dart';
+
+
+class CartScreen extends  StatelessWidget {
+  const CartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Container(
+          margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+          height: 44,
+          width: 68,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            border: Border.all(width: 1, color: Color(0xffcccccc)),
+          ),
+          child: Icon(
+            Icons.arrow_back_ios_new,
+          ),
+        ),
+        title: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Carrinho ',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            Image(
+              width: 16,
+              height: 16,
+              image: AssetImage('images/carrinho.png'),
+            ),
+          ],
+        ),
+        actions: [
+          Container(
+            height: 54,
+            width: 54,
+            decoration: BoxDecoration(
+              color: Colors.transparent,
+            ),
+            child: Icon(
+              Icons.search,
+              color: Colors.transparent,
+            ),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(24, 0, 24, 0),
+        child: Column(
+          children: [
+            RowBoxSell(
+              img: AssetImage('images/box_sell/carne.png'),
+              price: '40',
+              product: 'Carne',
+            ),
+            RowBoxSell(
+              img: AssetImage('images/box_sell/pimentao.png'),
+              price: '7',
+              product: 'Pimentao',
+            ),
+            RowBoxSell(
+              img: AssetImage('images/box_sell/brocolis.png'),
+              price: '4',
+              product: 'Brocolis',
+            ),
+          ]
+        ),
+      ),
+    );
+  }
+}
+
+class RowBoxSell extends StatelessWidget {
+  const RowBoxSell({
+    super.key,
+    required this.product,
+    required this.price,
+    required this.img,
+  });
+
+  final String product;
+  final String price;
+  final AssetImage img;
+
+  @override
+  Widget build(BuildContext  context){
+
+    return Container( // Widget que sera externalizado
+
+      margin: EdgeInsets.fromLTRB(0, 24, 0, 0),
+      width: 390,
+      height: 62,
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            width: 1,
+            color: Color(0xffcccccc),
+
+          ),
+        ),
+      ),
+      child: 
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children:[
+            Container(
+              child: Row(
+                children:[
+                  Image(
+                    image: img,
+                    width: 42,
+                    height: 42,
+                  ),
+                  SizedBox(width: 16,),
+
+                  Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                          Text(
+                            product,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          ),
+                          Text(
+                            '1kg, ${price}R\$',
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xffFF324B),
+                            ),
+                          ),
+                        ],
+                    ),
+                  ),
+                ]
+              ),
+            ),
+
+            Container(
+              width: 116,
+              child: Row(
+                children: [
+                  Container(
+                    width: 36,
+                    height: 36,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25),
+                      color: Color(0xffF3F5F7),
+                    ),
+                    child: Icon(
+                      Icons.add,
+                      color: Color(0xff979899),
+                    ),
+                  ),
+                  SizedBox(width: 16,),
+                  Container(
+                    child: Text(
+                      '1',
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 16,),
+                  Container(
+                    width: 36,
+                    height: 36,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25),
+                      color: Color(0xff23AA49),
+                    ),
+                    child: Icon(
+                      Icons.add,
+                      color: Color(0xffffffff),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        )
+    ); // Widget que sera externalizado
+  }
+}
